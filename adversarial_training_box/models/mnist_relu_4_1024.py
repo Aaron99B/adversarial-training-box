507ea500541633644c1acfcea75162dd7c92ddad
Aaron Berger <aaronberger@hotmail.de>
2024-05-28 15:43:27 +0200

    added network name

--- adversarial_training_box/models/mnist_relu_4_1024.py
+++ adversarial_training_box/models/mnist_relu_4_1024.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 class MNIST_RELU_4_1024(nn.Module):
     def __init__(self):
         super(MNIST_RELU_4_1024, self).__init__()
+        self.name = "mnist_relu_4_1024"
         self.layer1 = nn.Linear(784, 1024)
         self.layer2 = nn.Linear(1024, 1024)
         self.layer3 = nn.Linear(1024, 1024)
