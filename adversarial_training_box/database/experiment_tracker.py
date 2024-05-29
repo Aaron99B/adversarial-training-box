5af4b892f12f1e731c97baeca22742a74ca9fc34
Aaron Berger <aaronberger@hotmail.de>
2024-05-29 10:37:45 +0200

    enabled continuing of experiments

--- adversarial_training_box/database/experiment_tracker.py
+++ adversarial_training_box/database/experiment_tracker.py
@@ -3,40 +3,51 @@ import torch
 from pathlib import Path
 import pandas as pd
 from datetime import datetime
+from onnx2torch import convert
 
 from adversarial_training_box.database.attribute_dict import AttributeDict
 
 class ExperimentTracker:
     def __init__(self, project: str, base_path: Path, training_parameters: AttributeDict, login: bool) -> None:
         self.training_parameters = training_parameters
-
-        network_name = training_parameters.network
+        
+        self.logged_in = login
+        self.project = project
+        self.project_path = base_path / project 
+    
+    def initialize_new_experiment(self, experiment_name: str):
         now = datetime.now()
         now_string = now.strftime("%d-%m-%Y+%H_%M")
-        run_string = f"{network_name}_{now_string}"
-        if login:
+        run_string = f"{experiment_name}_{now_string}"
+        if self.logged_in:
             self.run = wandb.init(
             # set the wandb project where this run will be logged
-            project=project,
+            project=self.project,
             # track hyperparameters and run metadata
-            config=training_parameters,
+            config=self.training_parameters,
             name = run_string
             )
 
         self.experiment_name = run_string
-        
-        self.logged_in = login
 
-        self.path = base_path / project / self.experiment_name
-        if not self.path.exists():
-         self.path.mkdir(parents=True)
+        self.act_experiment_path = self.project_path / self.experiment_name
+
+        if not self.act_experiment_path.exists():
+            self.act_experiment_path.mkdir(parents=True)
+
+    def load(self, experiment_name: Path):
+        self.act_experiment_path = self.project_path / experiment_name
+    
+    def load_model(self, network_name: str) -> torch.nn.Module:
+        torch_model = convert(str(self.act_experiment_path / network_name))
+        return torch_model
     
     def log(self, information: dict) -> None:
         if self.logged_in:
             wandb.log(information)
 
     def save_model(self, network, data, upload_model=False) -> None:
-        model_path = self.path / f"{network.name}.onnx"
+        model_path = self.act_experiment_path / f"{network.name}.onnx"
         torch.onnx.export(network, data, model_path)
         
         if upload_model:
@@ -54,7 +65,7 @@ class ExperimentTracker:
        wandb.log({"class_wise_accuracy" : table})
 
     def log_test_result(self, result: dict):
-        result_df_path = self.path / "accuracy_table.csv"
+        result_df_path = self.act_experiment_path / "accuracy_table.csv"
         if result_df_path.exists():
             df = pd.read_csv(result_df_path, index_col=0)
             df.loc[len(df.index)] = result
@@ -63,11 +74,11 @@ class ExperimentTracker:
         df.to_csv(result_df_path)
 
     def log_table_result_table_online(self):
-        result_df_path = self.path / "accuracy_table.csv"
+        result_df_path = self.act_experiment_path / "accuracy_table.csv"
         df = pd.read_csv(result_df_path, index_col=0)
 
         if self.logged_in:
             table = wandb.Table(dataframe=df)
             wandb.log({"accuracy_table" : table})
         else:
-            print("[WARNING] results can't be logged online. not logged in.")
+            print("[WARNING] results can't be logged online. Login is set to false")
