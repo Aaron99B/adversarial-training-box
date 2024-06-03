d34bf625456787137defeb9e3a1839c5241b8d1d
Aaron Berger <aaronberger@hotmail.de>
2024-06-03 12:10:42 +0200

    repaired networks for onnx export

--- adversarial_training_box/models/conv_madry.py
+++ adversarial_training_box/models/conv_madry.py
@@ -16,10 +16,10 @@ class CNNMADRY(torch.nn.Module):
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x,2,2)
+        x = F.max_pool2d(x,(2,2))
         x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2,2)
-        x = x.reshape(-1, 7 * 7 * 64)
+        x = F.max_pool2d(x, (2,2))
+        x = x.view(-1, 7 * 7 * 64)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
