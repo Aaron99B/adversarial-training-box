4be1cba29b481c16df00e9962e9a49166a0bac45
Aaron Berger <aaronberger@hotmail.de>
2024-06-12 16:53:46 +0200

    repaired models for onnx export

--- adversarial_training_box/models/cnn_yang.py
+++ adversarial_training_box/models/cnn_yang.py
@@ -6,8 +6,8 @@ import torch.nn.functional as F
 
 class CNNYANG(nn.Module):
     def __init__(self):
-        super(CNNYANG, self).__init__()
         self.name = "cnn_yang"
+        super(CNNYANG, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, 3)
         self.conv2 = nn.Conv2d(32, 32, 3)
         self.conv3 = nn.Conv2d(32, 64, 3)
