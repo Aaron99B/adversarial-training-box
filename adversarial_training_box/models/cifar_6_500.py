afd004ce5831493246bac7d8805ebacadfa1994d
Aaron Berger <aaronberger@hotmail.de>
2024-07-26 00:02:07 +0200

    added cifar 6 500 network

---
+++ adversarial_training_box/models/cifar_6_500.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CIFAR_6_500(nn.Module):
+    def __init__(self):
+        super(CIFAR_6_500, self).__init__()
+
+        self.fc1 = nn.Linear(3072, 500)
+        self.fc2 = nn.Linear(500, 500)
+        self.fc3 = nn.Linear(500, 500)
+        self.fc4 = nn.Linear(500, 500)
+        self.fc5 = nn.Linear(500, 500)
+        self.fc6 = nn.Linear(500, 500)
+        self.fc7 = nn.Linear(500, 10)
+
+    def forward(self, x):
+        x = x.reshape(-1, 3072)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = F.relu(self.fc5(x))
+        x = F.relu(self.fc6(x))
+        x = self.fc7(x)
+
+        return x
