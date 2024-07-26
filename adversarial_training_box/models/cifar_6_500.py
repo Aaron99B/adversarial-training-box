ded4fd4e19510de4eccaa1558fa615eff1448ad1
Aaron Berger <aaronberger@hotmail.de>
2024-07-26 15:28:44 +0200

    added name

--- adversarial_training_box/models/cifar_6_500.py
+++ adversarial_training_box/models/cifar_6_500.py
@@ -1,10 +1,10 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 class CIFAR_6_500(nn.Module):
     def __init__(self):
         super(CIFAR_6_500, self).__init__()
+        self.name = "cifar_6_500"
 
         self.fc1 = nn.Linear(3072, 500)
         self.fc2 = nn.Linear(500, 500)
@@ -15,7 +15,7 @@ class CIFAR_6_500(nn.Module):
         self.fc7 = nn.Linear(500, 10)
 
     def forward(self, x):
-        x = x.reshape(-1, 3072)
+        x = x.view(-1, 3072)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
