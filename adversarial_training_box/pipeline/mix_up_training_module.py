84c356fda2eef1ac3aa54de7053cb209c344e9af
Aaron Berger <aaronberger@hotmail.de>
2024-07-30 23:24:23 +0200

    got mixup running

--- adversarial_training_box/pipeline/mix_up_training_module.py
+++ adversarial_training_box/pipeline/mix_up_training_module.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 from adversarial_training_box.pipeline.training_module import TrainingModule
 from adversarial_training_box.database.experiment_tracker import ExperimentTracker
@@ -7,19 +8,22 @@ from adversarial_training_box.adversarial_attack.adversarial_attack import Adver
 class MixUpTrainingModule(TrainingModule):
     """ Code for mixup function from https://github.com/locuslab/robust_overfitting/tree/master """
 
-    def __init__(self, criterion: torch.nn.Module, lambda_value: float) -> None:
+    def __init__(self, criterion: torch.nn.Module, alpha: float) -> None:
         self.criterion = criterion
-        self.lambda_value = lambda_value
+        self.alpha = alpha
 
-    def mixup_data(self, data: torch.Tensor, target: torch.Tensor, lambda_value):
+    def mixup_data(self, data: torch.Tensor, target: torch.Tensor, alpha: float):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        lambda_value = np.random.beta(alpha, alpha)
 
         batch_size = data.size()[0]
         index = torch.randperm(batch_size).to(device)
 
         mixed_data = lambda_value * data + (1 - lambda_value) * data[index, :]
+        mixed_data.to(device)
         target_a, target_b = target, target[index]
-        return mixed_data, target_a, target_b
+        return mixed_data, target_a, target_b, lambda_value
 
     def mixup_criterion(self, criterion, output, target_a, target_b, lambda_value):
         return lambda_value * criterion(output, target_a) + (1 - lambda_value) * criterion(output, target_b)
@@ -32,13 +36,14 @@ class MixUpTrainingModule(TrainingModule):
         for batch_idx, (data, target) in enumerate(data_loader):
             data, target = data.to(device), target.to(device)
 
-            mixed_data, target_a, target_b  = self.mixup_data(data, target, self.lambda_value)
+            mixed_data, target_a, target_b, lambda_value  = self.mixup_data(data, target, self.alpha)
 
             output = network(mixed_data)
-            loss = self.mixup_criterion(self.criterion, output, target_a, target_b, self.lambda_value)
+            loss = self.mixup_criterion(self.criterion, output, target_a, target_b, lambda_value)
+
+            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            optimizer.zero_grad()
 
             if not experiment_tracker is None:
                 experiment_tracker.log({"loss": loss.item()})
@@ -48,5 +53,5 @@ class MixUpTrainingModule(TrainingModule):
 
 
     def __str__(self) -> str:
-        return f"standard_module_{self.criterion}_{self.lambda_value}"
+        return f"mixup_module_{self.criterion}_{self.alpha}"
     
