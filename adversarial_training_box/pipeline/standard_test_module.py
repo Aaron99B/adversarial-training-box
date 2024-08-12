42266b57cdaade7612b0dafdceea93bb7f776073
Aaron Berger <aaronberger@hotmail.de>
2024-08-12 12:01:15 +0200

    updated testing for robust accuracy

--- adversarial_training_box/pipeline/standard_test_module.py
+++ adversarial_training_box/pipeline/standard_test_module.py
@@ -18,21 +18,30 @@ class StandardTestModule(TestModule):
         for data, target in data_loader:
             data, target = data.to(device), target.to(device)
 
-            if not self.attack is None:
-                data = self.attack.compute_perturbed_image(network=network, data=data, labels=target, epsilon=self.epsilon)
-
             output = network(data)
 
-            _, final_pred = output.data.max(1, keepdim=True)
-
-            correct += final_pred.eq(target.data.view_as(final_pred)).sum().item()
+            _, pred = output.data.max(1, keepdim=True)
 
             total += target.size(0)
+
+            if not self.attack is None:
+                correct_predictions = data[pred.eq(target.data.view_as(pred)).view_as(target)]
+                labels_for_correct_predictions = target[pred.eq(target.data.view_as(pred)).view_as(target)]
+                perturbed_data = self.attack.compute_perturbed_image(network=network, data=correct_predictions, labels=labels_for_correct_predictions, epsilon=self.epsilon)
+
+                output = network(perturbed_data)
+                _, adv_pred = output.data.max(1, keepdim=True)
+
+                correct += adv_pred.eq(labels_for_correct_predictions.data.view_as(adv_pred)).sum().item()
+
+            else:
+
+                correct += pred.eq(target.data.view_as(pred)).sum().item()
 
         final_acc = correct / total
 
         return self.attack, self.epsilon, final_acc
 
     def __str__(self) -> str:
-        return f"test_module_{self.attack}_{self.epsilon}"
+        return f"test_module_filtered_adv_acc{self.attack}_{self.epsilon}"
     
